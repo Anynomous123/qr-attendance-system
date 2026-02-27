a634d4409d54906e3eafc2aeda464dedf7765c03
Lalit Kumar <lalitbijj409@gmail.com>
2026-02-27 11:36:14 +0530

    Google_API_Replaced_with_Sqlit3 + New Classes+ extra plot 75 + Class wise + separate subject viewmore+repeat+error+otherissues+subjectlist

--- attendance_app.py
+++ attendance_app.py
@@ -140,13 +140,27 @@ if st.session_state.logged_in:
         ["B.Sc 1", "B.Sc 2", "B.Sc 3"]
     )
 
-    subjects = [
-        "Mechanics (PHYS101TH)",
-        "Modern Physics (PHYS301TH)",
-        "Nuclear and Particle Physics (PHYS304TH)"
-    ]
+	# ================= SUBJECT MAPPING BY CLASS =================
+	class_subjects = {
+		"B.Sc 1": [
+			"Mechanics (PHYS101TH)",
+			"Electricity & Magnetism (PHYS102TH)"
+		],
+		"B.Sc 2": [
+			"Waves & Optics (PHYS202TH)",
+			"Statistical Physics (PHYS201TH)"
+		],
+		"B.Sc 3": [
+			"Modern Physics (PHYS301TH)",
+			"Nuclear & Particle Physics (PHYS304TH)"
+		]
+	}
 
-    subject = st.sidebar.selectbox("Select Subject", subjects)
+
+	subjects = class_subjects[selected_class]
+
+
+	subject = st.sidebar.selectbox("Select Subject", subjects)
     duration = st.sidebar.number_input("QR Valid Duration (minutes)", 1, 60, 5)
 
     # ================= GENERATE QR =================
